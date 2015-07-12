45dff5c146df7b5d2aa987d23cd5d186e6bb121c
CasanovaTapia <ching@casanovatapia.com>
2015-07-12 14:58:26 -0700

    Temp root

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,7 @@
 class StaticController < ApplicationController
 	def about
 	end
+
+	def temp
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'static#about'
+  root 'static#temp'
 
-  get 'about', to: 'static#about', as: :static_about
+  # get 'about', to: 'static#about', as: :static_about
 end
