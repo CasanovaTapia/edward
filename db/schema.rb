92479ca06a093f32a0e1d0b7286f043b3fdd9066
CasanovaTapia <ching@casanovatapia.com>
2015-07-14 18:49:38 -0700

    Added posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150715012750) do
+ActiveRecord::Schema.define(version: 20150715013729) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.string   "body"
+    t.integer  "user_id"
+    t.integer  "category_id", default: 1
+    t.integer  "status_id",   default: 1
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["category_id"], name: "index_posts_on_category_id"
+  add_index "posts", ["status_id"], name: "index_posts_on_status_id"
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "statuses", force: true do |t|
     t.string   "name"
