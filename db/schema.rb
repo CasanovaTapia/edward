f424050e697eb1c5fa8ffdeef3d2179db9413b1f
CasanovaTapia <ching@casanovatapia.com>
2015-07-14 18:29:33 -0700

    status

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150713004847) do
+ActiveRecord::Schema.define(version: 20150715012750) do
 
   create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "statuses", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@ Category.create! id: 2, name: "touch"
 Category.create! id: 3, name: "hear"
 Category.create! id: 4, name: "taste"
 Category.create! id: 5, name: "smell"
+
+# Create Status
+Status.delete_all
+Status.create! id: 1, name: "draft"
+Status.create! id: 2, name: "pending"
+Status.create! id: 3, name: "published"
